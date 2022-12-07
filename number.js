3c2b01c0b9b94cf3441c96fdd23867b7693bcd26
priyanshumasai21 <118160381+priyanshumasai21@users.noreply.github.com>
2022-12-07 15:03:24 +0530

    Add files via upload
    hi bva

---
+++ number.js
@@ -0,0 +1,9 @@
+let num=9;
+let count=0;
+for(let i=0;i<=num;i++){
+    if(num%i==0){
+        count++
+    }
+}if(count=2){
+    console.log("it is prime number")
+}else{  console.log("it is  not prime number")}
